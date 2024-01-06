582a748b4f8473cc964d30c86535a1b1ef1da6d8
vanessasara <127692218+vanessasara@users.noreply.github.com>
2024-01-07 00:17:31 +0500

    Add files via upload

--- inquierer.js
+++ inquierer.js
@@ -1,38 +1,24 @@
 "use strict";
+// npm i inquirer 
+// @types/inquirer 
+// install this before coding 
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const inquirer_1 = __importDefault(require("inquirer"));
 (async () => {
-    let user = await inquirer_1.default.prompt([
-        {
-            type: "number",
-            name: "firstNumber",
-            message: "Kindly enter your first number"
-        },
-        {
-            type: "number",
-            name: "secondNumber",
-            message: "Kindly enter your second number"
-        },
-        {
-            type: "list",
-            name: "operator",
-            message: "Select your operator",
-            choices: ["+", "-", "*", "/"]
-        },
-    ]);
-    console.log(user);
-    if (user.operator === "+")
-        console.log(`Your answer of ${user.firstNumber + user.secondNumber}`);
-    else if (user.operator === "-")
-        console.log(`Your answer of ${user.firstNumber - user.secondNumber}`);
-    else if (user.operator === "*")
-        console.log(`Your answer of ${user.firstNumber * user.secondNumber}`);
-    else if (user.operator === "/")
-        console.log(`Your answer of ${user.firstNumber / user.secondNumber}`);
+    const systemGeneratedNo = Math.floor(Math.random() * 10) + 1;
+    const { userGuess } = await inquirer_1.default.prompt({
+        type: "number",
+        name: "userGuess",
+        message: "Guess the number (between 1 and 10):"
+    });
+    // Check if the user's guess matches the system-generated number
+    if (userGuess === systemGeneratedNo) {
+        console.log("Congratulations! You guessed it right.");
+    }
     else {
-        console.log("Please select a valid operator (+, -, *, /)");
+        console.log(`Oops! The correct number was ${systemGeneratedNo}. Better luck next time!`);
     }
 })();
